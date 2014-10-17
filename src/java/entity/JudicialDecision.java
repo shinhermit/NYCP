82f30ea1e7ccbd28d610b82623d0886fdd042570
CotCotCoder <emilien.arino@gmail.com>
2014-10-17 20:06:19 +0200

    Commenting out code in JudicialDecision.java JudicialDecisionPK.jav Conviction.java.

--- src/java/entity/JudicialDecision.java
+++ src/java/entity/JudicialDecision.java
@@ -1,81 +1,81 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package entity;
-
-import entity.primaryKeys.JudicialDecisionPK;
-import java.io.Serializable;
-import java.util.Date;
-import java.util.Objects;
-import javax.persistence.EmbeddedId;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
-/**
- *
- * @author milou
- */
-@Entity
-abstract class JudicialDecision implements Serializable {
-    private static final long serialVersionUID = 1L;
-    @EmbeddedId    
-    private JudicialDecisionPK judicialDecisionPK;
-
-    public JudicialDecision () {
-        
-    }
-    
-    public JudicialDecision (JudicialDecisionPK judicialDecisionPK) {
-        this.setJudicialDecisionPK(judicialDecisionPK);
-    }    
-
-    public JudicialDecision (String decisionTypeNumber, String prisonFileNumber, Date dateOfDecision) {
-        this.setJudicialDecisionPK(decisionTypeNumber, prisonFileNumber, dateOfDecision);
-    }    
-    
-    public JudicialDecisionPK judicialDecisionPK() {
-        return judicialDecisionPK;
-    }
-
-    public void setJudicialDecisionPK(JudicialDecisionPK judicialDecisionPK) {
-        this.judicialDecisionPK = judicialDecisionPK;
-    }
-
-    public void setJudicialDecisionPK(String decisionTypeNumber, String prisonFileNumber, Date dateOfDecision) {
-        this.judicialDecisionPK = new JudicialDecisionPK(decisionTypeNumber, prisonFileNumber, dateOfDecision);
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 7;
-        hash = 53 * hash + Objects.hashCode(this.judicialDecisionPK);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final JudicialDecision other = (JudicialDecision) obj;
-        if (!Objects.equals(this.judicialDecisionPK, other.judicialDecisionPK)) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "JudicialDecision{" + "judicialDecisionPK=" + judicialDecisionPK + '}';
-    }
-
-
-    
-}
+///*
+// * To change this license header, choose License Headers in Project Properties.
+// * To change this template file, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//package entity;
+//
+//import entity.primaryKeys.JudicialDecisionPK;
+//import java.io.Serializable;
+//import java.util.Date;
+//import java.util.Objects;
+//import javax.persistence.EmbeddedId;
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
+//
+///**
+// *
+// * @author milou
+// */
+//@Entity
+//abstract class JudicialDecision implements Serializable {
+//    private static final long serialVersionUID = 1L;
+//    @EmbeddedId    
+//    private JudicialDecisionPK judicialDecisionPK;
+//
+//    public JudicialDecision () {
+//        
+//    }
+//    
+//    public JudicialDecision (JudicialDecisionPK judicialDecisionPK) {
+//        this.setJudicialDecisionPK(judicialDecisionPK);
+//    }    
+//
+//    public JudicialDecision (String decisionTypeNumber, String prisonFileNumber, Date dateOfDecision) {
+//        this.setJudicialDecisionPK(decisionTypeNumber, prisonFileNumber, dateOfDecision);
+//    }    
+//  
+//    public JudicialDecisionPK getJudicialDecisionPK() {
+//        return judicialDecisionPK;
+//    }
+//
+//    public void setJudicialDecisionPK(JudicialDecisionPK judicialDecisionPK) {
+//        this.judicialDecisionPK = judicialDecisionPK;
+//    }
+//
+//    public void setJudicialDecisionPK(String decisionTypeNumber, String prisonFileNumber, Date dateOfDecision) {
+//        this.judicialDecisionPK = new JudicialDecisionPK(decisionTypeNumber, prisonFileNumber, dateOfDecision);
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        int hash = 7;
+//        hash = 53 * hash + Objects.hashCode(this.judicialDecisionPK);
+//        return hash;
+//    }
+//
+//    @Override
+//    public boolean equals(Object obj) {
+//        if (obj == null) {
+//            return false;
+//        }
+//        if (getClass() != obj.getClass()) {
+//            return false;
+//        }
+//        final JudicialDecision other = (JudicialDecision) obj;
+//        if (!Objects.equals(this.judicialDecisionPK, other.judicialDecisionPK)) {
+//            return false;
+//        }
+//        return true;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "JudicialDecision{" + "judicialDecisionPK=" + judicialDecisionPK + '}';
+//    }
+//
+//
+//    
+//}
--- src/java/entity/primaryKeys/JudicialDecisionPK.java
+++ src/java/entity/primaryKeys/JudicialDecisionPK.java
@@ -1,114 +1,114 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-package entity.primaryKeys;
-
-import java.io.Serializable;
-import java.util.Date;
-import java.util.Objects;
-import javax.persistence.Basic;
-import javax.persistence.Column;
-import javax.persistence.Embeddable;
-import javax.persistence.Temporal;
-import static javax.persistence.TemporalType.DATE;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-
-@Embeddable
-public class JudicialDecisionPK implements Serializable {
-    @Basic(optional = false)
-    @NotNull
-    @Size(min = 1, max = 1)
-    @Column(name = "DECISION_TYPE_NUMBER")
-    private String decisionTypeNumber;
-    
-    @Basic(optional = false)    
-    @NotNull
-    @Size(min = 1, max = 10)
-    @Column(name = "PRISON_FILE_NUMBER")
-    private String prisonFileNumber;
-    
-    @Basic(optional = false)
-    @NotNull
-    @Column(name = "DATE_OF_DECISION")
-    @Temporal(DATE)
-    private Date dateOfDecision;
-
-    public JudicialDecisionPK() {
-    }
-
-    public JudicialDecisionPK(String decisionTypeNumber, String prisonFileNumber, Date dateOfDecision) {
-        this.decisionTypeNumber = decisionTypeNumber;
-        this.prisonFileNumber = prisonFileNumber;
-        this.dateOfDecision = dateOfDecision;
-    }
-
-    public String getDecisionTypeNumber() {
-        return decisionTypeNumber;
-    }
-
-    public void setDecisionTypeNumber(String decisionTypeNumber) {
-        this.decisionTypeNumber = decisionTypeNumber;
-    }
-
-    public String getPrisonFileNumber() {
-        return prisonFileNumber;
-    }
-
-    public void setPrisonFileNumber(String prisonFileNumber) {
-        this.prisonFileNumber = prisonFileNumber;
-    }
-
-    public Date getDateOfDecision() {
-        return dateOfDecision;
-    }
-
-    public void setDateOfDecision(Date dateOfDecision) {
-        this.dateOfDecision = dateOfDecision;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 7;
-        hash = 53 * hash + Objects.hashCode(this.decisionTypeNumber);
-        hash = 53 * hash + Objects.hashCode(this.prisonFileNumber);
-        hash = 53 * hash + Objects.hashCode(this.dateOfDecision);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final JudicialDecisionPK other = (JudicialDecisionPK) obj;
-        if (!Objects.equals(this.decisionTypeNumber, other.decisionTypeNumber)) {
-            return false;
-        }
-        if (!Objects.equals(this.prisonFileNumber, other.prisonFileNumber)) {
-            return false;
-        }
-        if (!Objects.equals(this.dateOfDecision, other.dateOfDecision)) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "JudicialDecisionPK{" 
-                + "decisionTypeNumber=" + decisionTypeNumber 
-                + ", prisonFileNumber=" + prisonFileNumber 
-                + ", dateOfDecision=" + dateOfDecision 
-        + '}';
-    }
-    
-
-    
-}
+///*
+// * To change this license header, choose License Headers in Project Properties.
+// * To change this template file, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//
+//package entity.primaryKeys;
+//
+//import java.io.Serializable;
+//import java.util.Date;
+//import java.util.Objects;
+//import javax.persistence.Basic;
+//import javax.persistence.Column;
+//import javax.persistence.Embeddable;
+//import javax.persistence.Temporal;
+//import static javax.persistence.TemporalType.DATE;
+//import javax.validation.constraints.NotNull;
+//import javax.validation.constraints.Size;
+//
+//@Embeddable
+//public class JudicialDecisionPK implements Serializable {
+//    @Basic(optional = false)
+//    @NotNull
+//    @Size(min = 1, max = 1)
+//    @Column(name = "DECISION_TYPE_NUMBER")
+//    private String decisionTypeNumber;
+//    
+//    @Basic(optional = false)    
+//    @NotNull
+//    @Size(min = 1, max = 10)
+//    @Column(name = "PRISON_FILE_NUMBER")
+//    private String prisonFileNumber;
+//    
+//    @Basic(optional = false)
+//    @NotNull
+//    @Column(name = "DATE_OF_DECISION")
+//    @Temporal(DATE)
+//    private Date dateOfDecision;
+//
+//    public JudicialDecisionPK() {
+//    }
+//
+//    public JudicialDecisionPK(String decisionTypeNumber, String prisonFileNumber, Date dateOfDecision) {
+//        this.decisionTypeNumber = decisionTypeNumber;
+//        this.prisonFileNumber = prisonFileNumber;
+//        this.dateOfDecision = dateOfDecision;
+//    }
+//
+//    public String getDecisionTypeNumber() {
+//        return decisionTypeNumber;
+//    }
+//
+//    public void setDecisionTypeNumber(String decisionTypeNumber) {
+//        this.decisionTypeNumber = decisionTypeNumber;
+//    }
+//
+//    public String getPrisonFileNumber() {
+//        return prisonFileNumber;
+//    }
+//
+//    public void setPrisonFileNumber(String prisonFileNumber) {
+//        this.prisonFileNumber = prisonFileNumber;
+//    }
+//
+//    public Date getDateOfDecision() {
+//        return dateOfDecision;
+//    }
+//
+//    public void setDateOfDecision(Date dateOfDecision) {
+//        this.dateOfDecision = dateOfDecision;
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        int hash = 7;
+//        hash = 53 * hash + Objects.hashCode(this.decisionTypeNumber);
+//        hash = 53 * hash + Objects.hashCode(this.prisonFileNumber);
+//        hash = 53 * hash + Objects.hashCode(this.dateOfDecision);
+//        return hash;
+//    }
+//
+//    @Override
+//    public boolean equals(Object obj) {
+//        if (obj == null) {
+//            return false;
+//        }
+//        if (getClass() != obj.getClass()) {
+//            return false;
+//        }
+//        final JudicialDecisionPK other = (JudicialDecisionPK) obj;
+//        if (!Objects.equals(this.decisionTypeNumber, other.decisionTypeNumber)) {
+//            return false;
+//        }
+//        if (!Objects.equals(this.prisonFileNumber, other.prisonFileNumber)) {
+//            return false;
+//        }
+//        if (!Objects.equals(this.dateOfDecision, other.dateOfDecision)) {
+//            return false;
+//        }
+//        return true;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "JudicialDecisionPK{" 
+//                + "decisionTypeNumber=" + decisionTypeNumber 
+//                + ", prisonFileNumber=" + prisonFileNumber 
+//                + ", dateOfDecision=" + dateOfDecision 
+//        + '}';
+//    }
+//    
+//
+//    
+//}
